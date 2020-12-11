81b39402bb947afb80f94d3250333183c0289da7
rami-sick <69608926+rami-sick@users.noreply.github.com>
2020-12-11 09:19:11 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -62,7 +62,7 @@ def music(bot, update):
 
 
 def main():
-    u = Updater('YOUR-TOKEN')
+    u = Updater('1378558437:AAEhYQYH5WDG40E3Sg8rOyajgBU3IHs6Kp8')
     dp = u.dispatcher
 
     dp.add_handler(CommandHandler("start", start))
